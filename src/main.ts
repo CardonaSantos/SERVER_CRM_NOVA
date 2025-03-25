980b3d3bd324cd0acf2ff7890b4f47c533205e18
Sants <106097059+CardonaSantos@users.noreply.github.com>
2025-03-24 18:04:26 -0600

    fallo?-2

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const port = process.env.PORT || 3000;
-
+//importar algo aqui
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Habilitar CORS correctamente
