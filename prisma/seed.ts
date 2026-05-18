28c985e4a202d0180fedad702889447fc97ffe32
Sants <106097059+CardonaSantos@users.noreply.github.com>
2026-05-18 15:54:22 -0600

    Primer commit a demo repo-2

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,18 +3,18 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  console.log('🌱 Iniciando seed de la base de datos...');
+  console.log(' Iniciando seed de la base de datos...');
 
   // Crear una empresa
   const empresa = await prisma.empresa.create({
     data: {
-      nombre: 'Nova Sistemas S.A.',
-      direccion: 'Jacaltenango, Huehuetenango, Zona 2',
-      telefono: '4001-7273',
-      pbx: '50255556789',
-      correo: 'novasistemassa@gmail.net',
-      sitioWeb: 'https://www.facebook.com/Novasistemasgt/?locale=es_LA',
-      nit: '1234567-8',
+      nombre: 'Asociación ADIPY',
+      direccion: 'Cantón Pozo, Concepción Huista 01322-Concepción, Guatemala',
+      telefono: '4045 0131',
+      pbx: '4045 0131',
+      correo: 'yamanonhadipy@hotmail.com',
+      sitioWeb: '',
+      nit: '',
       logo1: 'https://mi-servidor.com/logos/logo1.png',
       logo2: 'https://mi-servidor.com/logos/logo2.png',
       logo3: 'https://mi-servidor.com/logos/logo3.png',
