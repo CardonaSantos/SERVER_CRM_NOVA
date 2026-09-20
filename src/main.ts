ce81937eb49d018c76a8b8cc613f69fea29c1b6a
CardonaSantos <socoklocz@gmail.com>
2026-09-20 17:39:59 -0600

    ajuste de cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     'http://localhost:5173',
     'http://localhost:8081',
     'https://nova-crm-server-test-ui-production.up.railway.app',
+    'https://pos-crm-nova.up.railway.app',
   ]);
 
   app.enableCors({
